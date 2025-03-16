05a5911ce7ba0d708a590854550788ce83f2cfd1
Cosmin Purcarea <cosmin.purcarea@outlook.com>
2025-03-16 11:28:02 +0100

    Update Clerk provider configuration in _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,21 +1,22 @@
+import '@/styles/globals.css';
 import { ClerkProvider } from '@clerk/nextjs';
-import '../styles/globals.css';
+import { dark } from '@clerk/themes';
 import CookieConsent from '../components/CookieConsent';
 
-function MyApp({ Component, pageProps }) {
+export default function App({ Component, pageProps }) {
   return (
     <ClerkProvider
       appearance={{
+        baseTheme: dark,
         elements: {
-          formButtonPrimary: "bg-blue-600 hover:bg-blue-700 text-white",
+          formButtonPrimary: 'bg-blue-600 hover:bg-blue-700 text-sm normal-case',
+          card: 'bg-white',
         }
       }}
-      navigate={(to) => window.location.href = to}
+      {...pageProps}
     >
       <Component {...pageProps} />
       <CookieConsent />
     </ClerkProvider>
   );
-}
-
-export default MyApp; 
+} 
